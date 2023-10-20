32a640a6232292682403137d94f89b9226caee51
lfilipe09 <luis.andrade987@gmail.com>
2023-10-20 16:12:03 -0300

    feat: create routes

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 const serverless = require("serverless-http");
 const express = require("express");
 const axios = require("axios");
+const { productsMapper, itemsMapper } = require("./utils");
 
 const app = express();
 const meliBaseURL = "https://api.mercadolibre.com";
-
-app.get("/", (req, res, next) => {
-  return res.status(200).json({
-    message: "Hello from root!",
-  });
-});
 
 app.get("/api/items", async (req, res, next) => {
   try {
@@ -18,26 +13,46 @@ app.get("/api/items", async (req, res, next) => {
       return res.status(400).json({ error: "Search query is required" });
     }
 
-    const encodedQuery = encodeURIComponent(query); // Encode query to handle special characters
+    const encodedQuery = encodeURIComponent(query); 
     const searchURL = `${meliBaseURL}/sites/MLB/search?q=${encodedQuery}`;
+
+    const categories = []
     
     const response = await axios.get(searchURL);
-    return res.status(200).json(response.data);
+
+    response.data.filters
+      .find(filter => filter.id === "category")?.values
+      ?.map((value) => value.path_from_root
+        .map(path => {
+          categories.push(path.name)
+        }))
+
+    const itemsResponse = itemsMapper(response, categories)
+
+    return res.status(200).json(itemsResponse);
   } catch (error) {
-    return res.status(500).json({ error: "Internal Server Error" });
+    return res.status(500).json({ error: `Internal Server Error` });
   }
 });
 
 app.get("/api/items/:id", async (req, res, next) => {
   try {
     const itemId = req.params.id;
-    console.log('itemId', itemId)
-    const itemURL = `${meliBaseURL}/items?ids=${itemId}`;
-    
+
+    const itemURL = `${meliBaseURL}/items?ids=${itemId}`;    
     const response = await axios.get(itemURL);
-    return res.status(200).json(response.data);
+
+    const categoryURL = `${meliBaseURL}/categories/${response.data[0].body.category_id}`;
+    const categoryResponse = await axios.get(categoryURL);
+
+    const categories = categoryResponse.data?.path_from_root?.map(path => path.name) || []
+    const productData = response.data[0].body
+
+    const itemResponse = productsMapper(categories, productData)
+
+    return res.status(200).json(itemResponse);
   } catch (error) {
-    return res.status(500).json({ error: "Internal Server Error" });
+    return res.status(500).json({ error: `Internal Server Error` });
   }
 });
 
